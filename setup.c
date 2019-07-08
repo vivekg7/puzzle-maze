b77524329ef055407d4daa533cf507e16642b984
Vivek Kumar Gupta <45590159+vivekg7@users.noreply.github.com>
2019-07-08 16:25:20 +0530

    Update setup.c

--- setup.c
+++ setup.c
@@ -3,8 +3,20 @@
 
 int main()
 {
+    // compile the c-code 
     system("gcc -o maze create_maze.c");
+    // run the c-code which creates data.txt file
     system("./maze");
-    system("python3 show_maze_color.py < data.txt");
+    // ask which python code to run
+    char c;
+    printf("\nPress Key 'g' and then Enter for black-n-white output\n");
+    printf("Else any Key or Enter for Colored output : ");
+    scanf("%c", &c);
+    if ( c == 'g' || c == 'G' ) {
+        system("python3 show_maze_gray.py < data.txt");
+    }
+    else {
+        system("python3 show_maze_color.py < data.txt");
+    }
     return 0;
 }
